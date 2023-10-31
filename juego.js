d95d44377468f444bcc5ff95421865121eae9f78
stiven muñoz usuga <stivenusuga019@gmail.com>
2023-10-30 20:03:08 -0500

    Se desarrolla primera parte visual del juego, se tienen recursos media

---
+++ juego.js
@@ -0,0 +1,8 @@
+let image1 = [
+    { nombre: "Juan Jose Gallego", url: "s/Jjose.jpg"},
+    { nombre: "Cantinflas", url: "images/Cantinflas.jpg"},
+    { nombre: "Yokoi Kenji", url: "images/kenji.jpg"},
+    { nombre: "Jorge Mario Aguirre", url: "images/JorgeAguila.jpg"},
+    { nombre: "Lokillo", url: "images/lokillo.jpeg"},
+    { nombre: "Julio Iglesias", url: "images/JulioIglesias.jpg"}
+]
