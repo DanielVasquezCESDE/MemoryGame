e88f68d45634d1060c60a173ac068ae5fb63c826
BBC lover <129465984+DanielVasquezCESDE@users.noreply.github.com>
2023-11-01 13:00:44 +0000

    Get Reck

--- juego.js
+++ juego.js
@@ -1,6 +1,6 @@
 let image1 = [
-    { nombre: "Juan Jose Gallego", url: "s/Jjose.jpg"},
-    { nombre: "Cantinflas", url: "images/Cantinflas.jpg"},
+    { nombre: "BBC lover", url: "s/Jjose.jpg"},
+    { nombre: "Negro", url: "images/Cantinflas.jpg"},
     { nombre: "Yokoi Kenji", url: "images/kenji.jpg"},
     { nombre: "Jorge Mario Aguirre", url: "images/JorgeAguila.jpg"},
     { nombre: "Lokillo", url: "images/lokillo.jpeg"},
